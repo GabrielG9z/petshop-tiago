a2620198484f4566841961a804b418f306eb6eeb
Gabriel Genovez <86001324+GabrielG9z@users.noreply.github.com>
2022-10-20 10:32:08 -0300

    update
    update

--- src/pages/sobre/Sobre.jsx
+++ src/pages/sobre/Sobre.jsx
@@ -3,6 +3,16 @@ const Sobre = () => {
   return (
     <section>
       <h2 className={estilos.titulo_secao}>Sobre nosso PetShop</h2>
+
+      <div>
+        <h3>Missão</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat
+          vero quod similique animi quasi et voluptas. Magnam quia velit,
+          perferendis saepe delectus blanditiis eum nemo libero veniam odit
+          reiciendis? Consequuntur!
+        </p>
+      </div>
     </section>
   );
 };
