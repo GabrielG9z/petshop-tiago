8833a40819f199ff3a7dbb5ccbce76b00dc247b7
Gabriel Genovez <86001324+GabrielG9z@users.noreply.github.com>
2022-10-28 11:13:36 -0300

    update
    Nessa branch fizemos o envio de dados para a api

--- src/api/servidor-api.js
+++ src/api/servidor-api.js
@@ -1,3 +1,4 @@
 const porta = "2112";
 const serverApi = `http://localhost:${porta}`;
+
 export default serverApi;
--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -1,6 +1,8 @@
 import { Button, TextField } from "@mui/material";
 import Caixa from "../../components/Caixa/Caixa";
 import estilos from "./Contato.module.css";
+import { useState } from "react";
+import serverApi from "../../api/servidor-api";
 const Contato = () => {
   /* Eventos/Funções para captura da digitação nos campos */
   const inputNome = (event) => setNome(event.target.value);
@@ -11,12 +13,41 @@ const Contato = () => {
   const [email, setEmail] = useState("");
   const [mensagem, setMensagem] = useState("");
 
+  const enviarContato = async (event) => {
+    event.preventDefault();
+
+    const opcoes = {
+      method: "POST",
+      body: JSON.stringify({ nome, email, mensagem }),
+      headers: {
+        "Content-type": "application/json; charset=utf8",
+      },
+    };
+    /* Script para envio dos dados para a API */
+
+    try {
+      await fetch(`${serverApi}/contatos`, opcoes);
+      alert("Dados enviados!");
+    } catch (error) {
+      console.log("Deu ruim: ".error.message);
+    }
+  };
+  let desabilitado = !nome || !email || !mensagem;
+  //let desabilitado = nome ==="" || email === "" ||mensagem === "";
+
+  /* "Toggle" do botão caso qualquer uma das variáveis seja undefined, desabilitado se manterá true e com isso o botão ficará desabilitado. */
+  /* Quando todas deixarem de ser undefined, desabilitado se tornará false e com isso o botão será habilitado */
+
   return (
     <section>
       <h2 className={estilos.titulo_secao}>Fale Conosco</h2>
 
       <Caixa>
-        <form method="post" className={estilos.formulario}></form>
+        <form
+          method="post"
+          className={estilos.formulario}
+          onSubmit={enviarContato}
+        ></form>
         <div>
           <TextField
             onChange={inputNome}
@@ -54,7 +85,7 @@ const Contato = () => {
         </div>
 
         <div>
-          <Button type="submit" variant="contained" onChange={inputMensagem}>
+          <Button type="submit" variant="contained" disabled={desabilitado}>
             ENVIAR MENSAGEM
           </Button>
         </div>
