4585f9be46cab2268f250105f8d8885218b8e734
Gabriel Genovez <86001324+GabrielG9z@users.noreply.github.com>
2022-11-01 08:51:35 -0300

    atualização da branch 11
    Fizemos uma programação para que o subtext sumisse ao campo ser preenchido

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -3,6 +3,7 @@ import Caixa from "../../components/Caixa/Caixa";
 import estilos from "./Contato.module.css";
 import { useState } from "react";
 import serverApi from "../../api/servidor-api";
+import { useHistory } from "react-router-dom";
 const Contato = () => {
   /* Eventos/Funções para captura da digitação nos campos */
   const inputNome = (event) => setNome(event.target.value);
@@ -13,9 +14,11 @@ const Contato = () => {
   const [email, setEmail] = useState("");
   const [mensagem, setMensagem] = useState("");
 
+  /* Hook necessário para criar uma navegação/redirecionamento manualmente (ou seja, sem depender dos routes) */
+  let history = useHistory();
+
   const enviarContato = async (event) => {
     event.preventDefault();
-
     const opcoes = {
       method: "POST",
       body: JSON.stringify({ nome, email, mensagem }),
@@ -28,6 +31,7 @@ const Contato = () => {
     try {
       await fetch(`${serverApi}/contatos`, opcoes);
       alert("Dados enviados!");
+      history.push("/"); //Redirecionar para a "home/blog"
     } catch (error) {
       console.log("Deu ruim: ".error.message);
     }
@@ -52,10 +56,10 @@ const Contato = () => {
           <TextField
             onChange={inputNome}
             label="Nome"
-            variant="filled"
+            variant="outlined"
             fullWidth
             required
-            helperText="Você deve digitar o seu nome"
+            helperText={!nome ? "Você deve digitar o seu nome" : ""}
           />
         </div>
         <div>
@@ -64,21 +68,21 @@ const Contato = () => {
               inputEmail
             } /* Só é preciso o nome da função no onChange sem o parenteses */
             label="Email"
-            variant="filled"
+            variant="outlined"
             fullWidth
             required
-            helperText="Informe seu e-mail para contato"
+            helperText={!email ? "Informe seu e-mail para contato" : ""}
           />
         </div>
         <div>
           <TextField
-            onChange={inputEmail}
+            onChange={inputMensagem}
             type="text"
             label="Mensagem"
             variant="outlined"
             fullWidth
             required
-            helperText="Digite uma sugestão"
+            helperText={!mensagem ? "Digite uma sugestão" : ""}
             multiline
             rows={6}
           />
