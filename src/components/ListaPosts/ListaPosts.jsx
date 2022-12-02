90dcc274b759c75ce846c190d62d7191ba96fde8
Gabriel Genovez <86001324+GabrielG9z@users.noreply.github.com>
2022-12-02 12:01:58 -0300

    update

--- src/components/ListaPosts/ListaPosts.jsx
+++ src/components/ListaPosts/ListaPosts.jsx
@@ -4,7 +4,8 @@ import serverApi from "../../api/servidor-api";
 import ArtigoPost from "../ArtigoPost/ArtigoPost";
 import LoadingDesenho from "../LoadingDesenho/LoadingDesenho";
 import estilos from "./ListaPosts.module.css";
-const ListaPosts = ({ url }) => {
+
+const ListaPosts = ({ categoria }) => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -36,6 +37,15 @@ const ListaPosts = ({ url }) => {
           };
 
           listaDePosts.push(objetoPost);
+          /* Se a categoria for escolhida/clicada */
+          if (categoria) {
+            /* Então vamos fazer uma lista de posts com filtro de categoria */
+            /* A cada vez que o loop for é executado, pegamos a categoria de cada post e comparamos com a categoria escolhida pelo usuário. */
+            listaDePosts = listaDePosts.filter(
+              /* Se esta comparação for verdadeira, guardamos o post na listaDePosts. Caso contrário, é descartado pelo filtro */
+              (cadaPost) => cadaPost.categoria === categoria
+            );
+          }
 
           console.log(objetoPost);
         }
@@ -51,7 +61,7 @@ const ListaPosts = ({ url }) => {
     
     Desta forma, o useEffect "entende" que ele deve executar novamente
     as suas ações (neste caso, executar novamente o fetch na API) */
-  }, [url]);
+  }, [categoria]);
 
   if (loading) {
     return <LoadingDesenho texto="posts..." />;
