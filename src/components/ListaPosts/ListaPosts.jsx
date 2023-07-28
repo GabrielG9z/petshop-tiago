b1ec6655cdc6a0825f797423cb8d022cf91eaa9a
GabrielG9z <genovezgabriel2012@gmail.com>
2023-07-27 23:20:11 -0300

    Update ListaPosts.jsx

--- src/components/ListaPosts/ListaPosts.jsx
+++ src/components/ListaPosts/ListaPosts.jsx
@@ -37,7 +37,7 @@ const ListaPosts = () => {
 
   return (
     <div className={estilos.lista_posts}>
-      {posts.map(({ id, titulo, subtitulo, }) => (
+      {posts.map(({ id, titulo, subtitulo }) => (
         <article className={estilos.post} key={id}>
           <h3> {titulo} </h3>
           <p>{subtitulo}</p>
