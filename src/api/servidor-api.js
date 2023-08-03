d8b05a7f259d2f927b028e0d9fd008057f4f26a7
GabrielG9z <genovezgabriel2012@gmail.com>
2023-08-03 17:04:47 -0300

    update

--- src/api/servidor-api.js
+++ src/api/servidor-api.js
@@ -1,4 +1,8 @@
 const porta = "2112";
 // const serverApi = `http://localhost:${porta}`; VERSÃO LOCALHOST
+
 const serverApi = `https://petshop-daf41-default-rtdb.firebaseio.com/`; //VERSAO REDELOCAL VIA IP
+
+//const serverApi = `http://192.168.15.12:${porta}`; //VERSAO REDELOCAL VIA IP
+
 export default serverApi;
